f236e518ab03d2fc9feb128a2e75d205ec39cf3c
TT0057 <deep005>
2017-11-02 21:16:29 +0530

    adding recipe model

--- src/app/recepie/recepies-list/recepies-list.component.ts
+++ src/app/recepie/recepies-list/recepies-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipes.model';
 
 @Component({
   selector: 'app-recepies-list',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recepies-list.component.css']
 })
 export class RecepiesListComponent implements OnInit {
+
+    recipes: Recipe[] = [
+      new Recipe('A test recipe','this is simply a test','http://maxpixel.freegreatpicture.com/static/photo/1x/' +
+        'Meat-Power-Recipe-Food-Dishes-Pork-1459693.jpg')
+    ];
 
   constructor() { }
 
