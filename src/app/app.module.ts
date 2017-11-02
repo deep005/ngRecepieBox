d4904fe1821a9356700c5014d87c268ba02ae442
TT0057 <deep005>
2017-11-02 10:21:04 +0530

    initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { RecepieComponent } from './recepie/recepie.component';
+import { RecepiesListComponent } from './recepie/recepies-list/recepies-list.component';
+import { RecepiesDetailsComponent } from './recepie/recepies-details/recepies-details.component';
+import { ItemsComponent } from './recepie/recepies-list/items/items.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    RecepieComponent,
+    RecepiesListComponent,
+    RecepiesDetailsComponent,
+    ItemsComponent,
+    ShoppingListComponent,
+    ShoppingEditComponent
   ],
   imports: [
     BrowserModule
