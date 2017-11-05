23960dea94885fbe1e73e396a9f1cfd61537adee
TT0057 <deep005>
2017-11-05 09:47:53 +0530

    sending data to the items component from the recipe-list component

--- src/app/recepie/recepies-list/items/items.component.ts
+++ src/app/recepie/recepies-list/items/items.component.ts
@@ -1,12 +1,12 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Input} from '@angular/core';
+import { Recipe } from '../../recipes.model';
 @Component({
   selector: 'app-items',
   templateUrl: './items.component.html',
   styleUrls: ['./items.component.css']
 })
 export class ItemsComponent implements OnInit {
-
+  @Input() recipe:Recipe;
   constructor() { }
 
   ngOnInit() {
