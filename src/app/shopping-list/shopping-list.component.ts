13276a5d3764140587e5c3270fb600a9f6110967
TT0057 <deep005>
2017-11-17 10:04:22 +0530

    adding shopping list service

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,22 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, DoCheck} from '@angular/core';
 import { Ingredient } from '../shared/ingredient.model';
-
+import { ShoppingListService } from './shoppingList.service';
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css']
 })
-export class ShoppingListComponent implements OnInit {
-  ingredients: Ingredient[] = [
-    new Ingredient('Apples', 5),
-    new Ingredient('Oranges', 7)
-  ];
-  constructor() { }
+export class ShoppingListComponent implements OnInit, DoCheck {
+  ingredients: Ingredient[];
+  constructor(private slService: ShoppingListService) {
+    console.log("Hello");
+  }
 
   ngOnInit() {
-  }
-  onIngredientAdded(ingredient: Ingredient){
-    this.ingredients.push(ingredient);
+    this.ingredients = this.slService.getIngredients();
+    this.slService.ingredientsChanged.subscribe(
+    (ingredients: Ingredient[])=>{
+      this.ingredients = ingredients;
+    });
   }
 
+  ngDoCheck(){
+    console.log("hello");
+  }
 }
--- src/app/shopping-list/shoppingList.service.ts
+++ src/app/shopping-list/shoppingList.service.ts
@@ -1,3 +1,19 @@
-export class RecipeListService{
-  
+import { Ingredient } from '../shared/ingredient.model';
+import { EventEmitter } from '@angular/core';
+export class ShoppingListService{
+
+  ingredientsChanged = new EventEmitter<Ingredient[]>();
+  private ingredients: Ingredient[] = [
+    new Ingredient('Apples', 5),
+    new Ingredient('Oranges', 7)
+  ];
+
+   addIngredient(ingredient: Ingredient){
+     this.ingredients.push(ingredient);
+     this.ingredientsChanged.emit(this.ingredients.slice());
+   }
+
+  getIngredients(){
+    return this.ingredients.slice();
+  }
 }
