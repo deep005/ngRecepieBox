59aa4a4e1057265f0d5dcbdd8d996df03dda3572
TT0057 <deep005>
2017-12-01 20:57:34 +0530

    adding delete clear and update functionality

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -10,25 +10,45 @@ import {Subscription} from "rxjs/Rx";
   styleUrls: ['./shopping-edit.component.css']
 })
 export class ShoppingEditComponent implements OnInit, OnDestroy{
-  @ViewChild('nameInput') nameInput: ElementRef;
-  @ViewChild('amountInput') amountInput: ElementRef;
+  @ViewChild('f') slForm: NgForm;
+
   subscription: Subscription;
   editMode: boolean = false;
   editedItemIndex: number;
+  editedItem: Ingredient;
 
   constructor(private shoppingListService: ShoppingListService) { }
 
   ngOnInit() {
     this.subscription = this.shoppingListService.startedEditing.subscribe((index: number) => {
       this.editedItemIndex = index;
-      this.editMode = true; 
+      this.editMode = true;
+      this.editedItem = this.shoppingListService.getIngredient(this.editedItemIndex);
+      this.slForm.setValue({
+        name: this.editedItem.name,
+        amount: this.editedItem.amount
+      })
     });
   }
-  onAddItem(form : NgForm){
+  onSubmit(form : NgForm){
     const value = form.value;
     const newIngredient = new Ingredient(value.name, value.amount);
-    this.shoppingListService.addIngredient(newIngredient);
-
+    if(this.editMode){
+      this.shoppingListService.updateIngredient(this.editedItemIndex, newIngredient);
+    }
+    else{
+      this.shoppingListService.addIngredient(newIngredient);
+    }
+    this.editMode = false;
+    form.reset();
+  }
+  onClear(){
+    this.editMode = false;
+    this.slForm.reset();
+  }
+  onDelete(){
+    this.shoppingListService.deleteIngredient(this.editedItemIndex);
+    this.onClear();
   }
   ngOnDestroy(){
     this.subscription.unsubscribe();
