3fb0981969d9bd3fd1455fe94192b8d5b77b9f21
TT0057 <deep005>
2017-12-11 15:58:15 +0530

    creating first features module for recipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,54 +1,40 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {HttpModule} from "@angular/http";
+
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { RecepieComponent } from './recepie/recepie.component';
-import { RecepiesListComponent } from './recepie/recepies-list/recepies-list.component';
-import { RecepiesDetailsComponent } from './recepie/recepies-details/recepies-details.component';
-import { ItemsComponent } from './recepie/recepies-list/items/items.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
-
-//directives
-import { DropdownDirective } from './shared/dropdown.directive';
 
 //services
 import { RecipeService } from './recepie/recipe.service';
 import { DataStorageService } from './shared/data-storage.service';
 import { ShoppingListService } from './shopping-list/shoppingList.service';
 import {AppRoutingModule} from "./app-routing-module";
-import { RecipeStartComponent } from './recepie/recipe-start/recipe-start.component';
-import { RecipeEditComponent } from './recepie/recipe-edit/recipe-edit.component';
-import {FormsModule, ReactiveFormsModule} from "@angular/forms";
-import {HttpModule} from "@angular/http";
+import {FormsModule} from "@angular/forms";
 import { SignupComponent } from './auth/signup/signup.component';
 import { SigninComponent } from './auth/signin/signin.component';
 import {AuthService} from "./auth/auth.service";
 import {AuthGuard}  from "./auth/auth-guard-service";
+import {RecipesModule} from "./recepie/recipe.module";
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    RecepieComponent,
-    RecepiesListComponent,
-    RecepiesDetailsComponent,
-    ItemsComponent,
     ShoppingListComponent,
     ShoppingEditComponent,
-    DropdownDirective,
-    RecipeStartComponent,
-    RecipeEditComponent,
     SignupComponent,
     SigninComponent
   ],
   imports: [
-    BrowserModule, 
+    BrowserModule,
     AppRoutingModule,
     FormsModule,
     HttpModule,
-    ReactiveFormsModule
+    RecipesModule
   ],
   providers: [RecipeService, ShoppingListService, DataStorageService, AuthService, AuthGuard],
   bootstrap: [AppComponent]
